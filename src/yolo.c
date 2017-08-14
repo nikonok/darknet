eb2d8e33b499d2ec6ffba01a3f370760bd83aff2
Anton <shkinder.anton@gmail.com>
2017-08-14 15:26:55 +0300

    Fixed yolo.c

--- src/yolo.c
+++ src/yolo.c
@@ -11,9 +11,9 @@
 //#include "ini.c"
 
 #ifdef OPENCV
-#pragma comment(lib, "opencv_core249.lib")  
-#pragma comment(lib, "opencv_imgproc249.lib")  
-#pragma comment(lib, "opencv_highgui249.lib")  
+//#pragma comment(lib, "opencv_core249.lib")  
+//#pragma comment(lib, "opencv_imgproc249.lib")  
+//#pragma comment(lib, "opencv_highgui249.lib")  
 //#pragma comment(lib, "opencv_gpu249.lib")  
 //#pragma comment(lib, "opencv_photo249.lib")  
 //#pragma comment(lib, "opencv_video249.lib")  
